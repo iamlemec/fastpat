b3577487d9e76554dc05aeeb2511047d61333c43
Doug Hanley <thesecretaryofwar@gmail.com>
2016-04-05 23:00:09 -0400

    add simcore pyx

---
+++ simcore.pyx
@@ -0,0 +1,34 @@
+DEF dim = 64
+
+cdef unsigned long masks[dim]
+for i in range(dim):
+  masks[i] = 1 << i
+
+cdef float v[dim]
+
+def simcore(hashish,weights):
+    cdef unsigned long ans
+    cdef int n = len(hashish)
+    cdef unsigned long h
+    cdef float w
+    cdef float q
+
+    for j in range(dim):
+        v[j] = 0.0
+
+    for i in range(n):
+        h = hashish[i]
+        w = weights[i]
+        for j in range(dim):
+            if h & masks[j]:
+              q = w
+            else:
+              q = -w
+            v[j] += q
+
+    ans = 0
+    for j in range(dim):
+        if v[j] >= 0:
+            ans |= masks[j]
+
+    return ans
