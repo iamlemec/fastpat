b5ddbd46f1280ca4086a74a10cfd9a5a7d80fbf6
Doug Hanley <thesecretaryofwar@gmail.com>
2016-05-11 13:21:09 -0400

    fetching

--- grant_files/fetch_grants.py
+++ grant_files/fetch_grants.py
@@ -4,7 +4,7 @@ import os
 import time
 
 grant_fname = 'grant_files.txt'
-grant_url_fmt = 'https://bulkdata.uspto.gov/data2/patent/grant/redbook/fulltext/{}/{}'
+grant_url_fmt = 'https://bulkdata.uspto.gov/data2/patent/grant/redbook/bibliographic/{}/{}'
 
 url_list = []
 for line in open(grant_fname):
@@ -19,10 +19,17 @@ for line in open(grant_fname):
     else:
         year = line[0:4]
 
-    url = grant_url_fmt.format(year,line.strip())
-    url_list.append(url)
+    fname = line
+    url = grant_url_fmt.format(year,line)
+    url_list.append((fname,url))
 
-for url in url_list:
-    os.system('wget ' + url)
-    time.sleep(10)
+for (fname,url) in sorted(url_list):
+    if os.path.isfile(fname):
+        print('Skipping {}, already exists'.format(fname))
+        print()
+    else:
+        print('Fetching {}'.format(fname))
+        os.system('curl -o {} {}'.format(fname,url))
+        print()
+        time.sleep(10)
 
