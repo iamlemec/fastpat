2d29b8d52e2e0060bc720fddad3817cd78fefeaa
Douglas Hanley <thesecretaryofwar@gmail.com>
2021-12-16 10:51:56 -0500

    reorg and simplify fetch

---
+++ patents/tools/fetch.py
@@ -0,0 +1,27 @@
+#!/bin/env python3
+
+import os
+import time
+
+def fetch_file(zurl, output, overwrite=False, dryrun=False):
+    system = print if dryrun else os.system
+    zflags = '' if args.overwrite else '-n'
+
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    _, zname = os.path.split(zurl)
+    zpath = os.path.join(output, zname)
+
+    if overwrite or not os.path.isfile(zpath):
+        print(f'Fetching {zname}')
+        system(f'curl -o {zpath} {zurl}')
+
+    print(f'Unzipping {zname}')
+    system(f'unzip {flags} {zpath} -d {output}')
+
+def fetch_many(files, output, delay=10, dryrun=False, **kwargs):
+    for zurl in files:
+        fetch_file(zurl, output, dryrun=dryrun, **kwargs)
+        if not dryrun:
+            time.sleep(delay)
